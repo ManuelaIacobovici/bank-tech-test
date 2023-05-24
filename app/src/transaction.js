a412ca9207d40b5eea6be9c446ad0d7b5860adec
Manuela Iacobovici <miacobovici@yahoo.co.uk>
2023-05-24 10:19:24 +0100

    Transaction Class is set up

---
+++ app/src/transaction.js
@@ -0,0 +1,9 @@
+class Transaction {
+  constructor( value, type ) {
+    this.value = value;
+    this.type = type;
+    this.date = new Date().toLocaleDateString('en-GB');
+  }
+}
+
+module.exports = Transaction;
